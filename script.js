e814cb4526485943222b2636d89f9e35a4672be9
SwaraliDoshi97 <swaralidoshi222@gmail.com>
2026-05-31 02:35:46 +0530

    fixed head section in mainline

--- script.js
+++ script.js
@@ -31,6 +31,23 @@ window.addEventListener("load", () => {
 });
 
 
+const cards = document.querySelectorAll('.card');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+        }
+    });
+}, {
+    threshold: 0.15
+});
+
+cards.forEach(card => {
+    observer.observe(card);
+});
+
 
 
 
