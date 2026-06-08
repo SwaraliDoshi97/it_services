84260f1388b9247aaa3b628b64818a27d36b9bdf
SwaraliDoshi97 <swaralidoshi222@gmail.com>
2026-06-08 21:51:30 +0530

    fixed hamburger js

--- script.js
+++ script.js
@@ -66,3 +66,17 @@ hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove("active");
+    });
+});
+
+document.addEventListener('click', (e) => {
+    if (
+        !navLinks.contains(e.target) &&
+        !hamburger.contains(e.target)
+    ) {
+        navLinks.classList.remove('active');
+    }
+});
